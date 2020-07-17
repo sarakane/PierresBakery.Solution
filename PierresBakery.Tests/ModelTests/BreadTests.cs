96a8fe6501609db05c3203466365b9909952810d
Sara Kane <kane.sarak@gmail.com>
2020-07-17 09:11:37 -0700

    Add test for creating an instance of the Bread class

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -6,10 +6,11 @@ namespace PierresBakery.Tests
   [TestClass]
   public class BreadTests
   {
-    /*[TestMethod]
-    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      
-    } */
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    } 
   }
 }
